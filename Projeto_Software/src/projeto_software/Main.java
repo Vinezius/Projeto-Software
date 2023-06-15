061f93ea01984021cb317b5e213ce1523f233e9f
Vinicius <vinicius.s.alves26@gmail.com>
2023-06-15 20:10:36 -0300

    Initial commit

---
+++ Projeto_Software/src/projeto_software/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto_software;
+
+import projeto_software.frames.Home;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Home home = new Home();
+        
+        home.setVisible(true);
+        
+    }
+    
+}
