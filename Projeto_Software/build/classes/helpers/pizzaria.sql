0f08f490a5ee1164f0c66f5a1c7345da33233852
Vinicius <vinicius.s.alves26@gmail.com>
2023-06-21 00:21:59 -0300

    dump database

--- Projeto_Software/build/classes/helpers/pizzaria.sql
+++ Projeto_Software/build/classes/helpers/pizzaria.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2023 at 02:31 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Tempo de geração: 21/06/2023 às 05:21
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,239 +18,288 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pizzaria`
+-- Banco de dados: `pizzaria`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cliente`
+-- Estrutura para tabela `cliente`
 --
 
 CREATE TABLE `cliente` (
   `idCliente` int(11) NOT NULL,
-  `nome` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `cpf` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `dataCadastro` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `endereco` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `telefone` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(15) DEFAULT NULL,
+  `dataCadastro` varchar(20) DEFAULT NULL,
+  `endereco` varchar(50) DEFAULT NULL,
+  `telefone` varchar(10) DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `nome`, `cpf`, `dataCadastro`, `endereco`, `telefone`, `ativo`) VALUES
+(1, 'teste', '10938086910', 'teste', 'teste', 'testes', 1),
+(2, 'testeCliente', '94856479123', '20/06/2023', 'teste', 'teste', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `entregador`
+-- Estrutura para tabela `entregador`
 --
 
 CREATE TABLE `entregador` (
   `idEntregador` int(11) NOT NULL,
-  `nome` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `cpf` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `placa` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(15) DEFAULT NULL,
+  `placa` varchar(10) DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `entregador`
+--
+
+INSERT INTO `entregador` (`idEntregador`, `nome`, `cpf`, `placa`, `ativo`) VALUES
+(1, 'teste', '80049782146', 'ABC1234', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `funcionario`
+-- Estrutura para tabela `funcionario`
 --
 
 CREATE TABLE `funcionario` (
   `idFuncionario` int(11) NOT NULL,
-  `nome` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `cargo` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cargo` varchar(25) DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `funcionario`
+-- Despejando dados para a tabela `funcionario`
 --
 
 INSERT INTO `funcionario` (`idFuncionario`, `nome`, `cargo`, `ativo`) VALUES
-(1, 'rafoles', 'rafaelto', 0);
+(2, 'teste funcionário', 'teste', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `modalidade`
+-- Estrutura para tabela `modalidade`
 --
 
 CREATE TABLE `modalidade` (
   `idModalidade` int(11) NOT NULL,
-  `modalidade` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `modalidade` varchar(50) DEFAULT NULL,
   `preco` decimal(4,2) DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Despejando dados para a tabela `modalidade`
+--
+
+INSERT INTO `modalidade` (`idModalidade`, `modalidade`, `preco`, `ativo`) VALUES
+(1, 'Entrega', 8.00, 1),
+(2, 'Retirada', 0.00, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedido`
+-- Estrutura para tabela `pedido`
 --
 
 CREATE TABLE `pedido` (
   `numPedido` int(11) NOT NULL,
-  `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `data` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hora` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `endereco` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `funcionario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `entregador` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `promocao` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `quantidade` int(20) NOT NULL,
-  `sabor` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tamanho` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `numFatias` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `acrescimo` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `observacoes` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `nome` varchar(50) NOT NULL,
+  `data` varchar(20) NOT NULL,
+  `hora` varchar(10) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `funcionario` varchar(50) NOT NULL,
+  `entregador` varchar(50) NOT NULL,
+  `promocao` varchar(50) NOT NULL,
+  `quantidade` varchar(50) NOT NULL,
+  `sabor` varchar(50) NOT NULL,
+  `tamanho` varchar(50) NOT NULL,
+  `numFatias` varchar(50) NOT NULL,
+  `acrescimo` varchar(20) NOT NULL,
+  `observacoes` varchar(50) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `pedido` varchar(50) NOT NULL,
+  `modalidade` varchar(50) NOT NULL,
+  `valorFinal` varchar(50) NOT NULL,
+  `placa` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `promocao`
+-- Estrutura para tabela `promocao`
 --
 
 CREATE TABLE `promocao` (
   `idPromocao` int(11) NOT NULL,
-  `promocao` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `promocao` varchar(50) DEFAULT NULL,
   `desconto` decimal(4,2) DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL,
-  `observacao` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `observacao` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `promocao`
+--
+
+INSERT INTO `promocao` (`idPromocao`, `promocao`, `desconto`, `ativo`, `observacao`) VALUES
+(2, 'Natal', 10.00, 1, 'natal');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sabor`
+-- Estrutura para tabela `sabor`
 --
 
 CREATE TABLE `sabor` (
   `idSabor` int(11) NOT NULL,
-  `sabor` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `observacao` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sabor` varchar(30) NOT NULL,
+  `observacao` varchar(50) NOT NULL,
   `ativo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `sabor`
+--
+
+INSERT INTO `sabor` (`idSabor`, `sabor`, `observacao`, `ativo`) VALUES
+(2, 'Calabresa', 'Calabresa', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tamanho`
+-- Estrutura para tabela `tamanho`
 --
 
 CREATE TABLE `tamanho` (
   `idTamanho` int(11) NOT NULL,
-  `tamanho` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `preco` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `numFatias` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `tamanho` varchar(20) NOT NULL,
+  `preco` varchar(20) NOT NULL,
+  `numFatias` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Despejando dados para a tabela `tamanho`
+--
+
+INSERT INTO `tamanho` (`idTamanho`, `tamanho`, `preco`, `numFatias`) VALUES
+(2, 'Pequena', '20,99', '6'),
+(3, 'Média', '30,99', '8');
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `cliente`
+-- Índices de tabela `cliente`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`idCliente`);
 
 --
--- Indexes for table `entregador`
+-- Índices de tabela `entregador`
 --
 ALTER TABLE `entregador`
   ADD PRIMARY KEY (`idEntregador`);
 
 --
--- Indexes for table `funcionario`
+-- Índices de tabela `funcionario`
 --
 ALTER TABLE `funcionario`
   ADD PRIMARY KEY (`idFuncionario`);
 
 --
--- Indexes for table `modalidade`
+-- Índices de tabela `modalidade`
 --
 ALTER TABLE `modalidade`
   ADD PRIMARY KEY (`idModalidade`);
 
 --
--- Indexes for table `pedido`
+-- Índices de tabela `pedido`
 --
 ALTER TABLE `pedido`
   ADD PRIMARY KEY (`numPedido`);
 
 --
--- Indexes for table `promocao`
+-- Índices de tabela `promocao`
 --
 ALTER TABLE `promocao`
   ADD PRIMARY KEY (`idPromocao`);
 
 --
--- Indexes for table `sabor`
+-- Índices de tabela `sabor`
 --
 ALTER TABLE `sabor`
   ADD PRIMARY KEY (`idSabor`);
 
 --
--- Indexes for table `tamanho`
+-- Índices de tabela `tamanho`
 --
 ALTER TABLE `tamanho`
   ADD PRIMARY KEY (`idTamanho`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `cliente`
+-- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `entregador`
+-- AUTO_INCREMENT de tabela `entregador`
 --
 ALTER TABLE `entregador`
-  MODIFY `idEntregador` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEntregador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `funcionario`
+-- AUTO_INCREMENT de tabela `funcionario`
 --
 ALTER TABLE `funcionario`
-  MODIFY `idFuncionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idFuncionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `modalidade`
+-- AUTO_INCREMENT de tabela `modalidade`
 --
 ALTER TABLE `modalidade`
-  MODIFY `idModalidade` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idModalidade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `pedido`
+-- AUTO_INCREMENT de tabela `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `numPedido` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `numPedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `promocao`
+-- AUTO_INCREMENT de tabela `promocao`
 --
 ALTER TABLE `promocao`
-  MODIFY `idPromocao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idPromocao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `sabor`
+-- AUTO_INCREMENT de tabela `sabor`
 --
 ALTER TABLE `sabor`
-  MODIFY `idSabor` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idSabor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `tamanho`
+-- AUTO_INCREMENT de tabela `tamanho`
 --
 ALTER TABLE `tamanho`
-  MODIFY `idTamanho` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTamanho` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
